98e0e4d13929715967ca81e4ef116aae31ef5555
Hanif <hanif.hakim@spesolution.com>
2023-01-16 20:12:56 +0700

    use t.Error and t.Fatal to fail the test.

--- helper/helllo_world_test.go
+++ helper/helllo_world_test.go
@@ -8,7 +8,10 @@ import (
 func TestHelloWorldHakim(t *testing.T) {
 	result := HelloWorld("Hakim")
 	if result != "Hello Hakim" {
-		t.Fail()
+		// t.Fail()
+
+		// t.Error will Fail() with message description
+		t.Error("Result must be Hello Hakim")
 	}
 
 	// t.Fail() will fail the test but continue rest of code inside this test block
@@ -20,8 +23,10 @@ func TestHelloWorldFauzi(t *testing.T) {
 	if result != "Hello Fauzi" {
 
 		// t.FailNow() will fail the test and end code cyle here.
-		t.FailNow()
+		// t.FailNow()
 
+		// t.Fatal will FailNow() with message description
+		t.Fatal("Result must be Hello Fauzi")
 		fmt.Println("This code will not be executed")
 
 	}
